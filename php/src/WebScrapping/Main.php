fa0aefb36fba43cc44faa7a55e83e9d3c302a0ee
Victor Rodrigues da Silva <victor.vki.dasilva@gmail.com>
2024-05-21 19:44:39 -0300

    writing in xlsx

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -25,8 +25,44 @@ class Main {
 
     // Write your logic to save the output file bellow.
     
-    print_r($data);
-    
+    //print_r($data);
+    $filename = __DIR__ . '/../../assets/result.xlsx';
+    $writer = WriterFactory::createFromType('xlsx');
+    $writer->openToFile($filename);
+
+    // Definindo os cabeçalhos das colunas
+    $headers = ['ID', 'Title', 'Type', 'Author1', 'Institution1', 'Author2', 'Institution2', 'Author3', 'Institution3', 'Author4', 'Institution4', 'Author5', 'Institution5', 'Author6', 'Institution6', 'Author7', 'Institution7', 'Author8', 'Institution8', 'Author9', 'Institution9'];
+    $rowFromValues = WriterEntityFactory::createRowFromArray($headers);
+    $writer->addRow($rowFromValues);
+
+    // Adicionando os dados
+    foreach ($data as $paper) {
+      $tmp_row = [
+          $paper->id,
+          $paper->title,
+          $paper->type
+      ];
+
+      // Adiciona autores e suas instituições
+      for ($i = 0; $i < 9; $i++) {
+          if (isset($paper->authors[$i])) {
+              $tmp_row[] = $paper->authors[$i]->name;
+              $tmp_row[] = $paper->authors[$i]->institution;
+          } else {
+              $tmp_row[] = '';
+              $tmp_row[] = '';
+          }
+      }
+
+      $rowFromValues = WriterEntityFactory::createRowFromArray($tmp_row);
+      $writer->addRow($rowFromValues);
+    }
+
+    // Fecha o writer
+    $writer->close();
+
+    echo "Dados exportados para o arquivo: $filename\n";
   }
+  
 
 }
